20ab31a40c94782093d7c5d65bb49eb94046c603
nyerneni <naresh_yerneni@intuit.com>
2017-03-02 11:42:35 -0600

    Initial Commit

---
+++ tipsy calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipsy calculator
+//
+//  Created by Yerneni, Naresh on 3/2/17.
+//  Copyright © 2017 Yerneni, Naresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
